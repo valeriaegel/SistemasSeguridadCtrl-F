b591ce65cfbe84c1ef39c11e6b92d526656b54c6
valeriaegel <valeriaegel@gmail.com>
2026-04-07 15:30:26 -0300

    Implementacion

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 
 const NavLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
   const pathname = usePathname();
@@ -34,6 +35,19 @@ export function Header() {
           <NavLink href="/students">Estudiantes</NavLink>
         </nav>
       </div>
+       <ClerkProvider>
+                  <Show when="signed-out">
+                    <SignInButton />
+                    <SignUpButton>
+                      <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                        Sign Up
+                      </button>
+                    </SignUpButton>
+                  </Show>
+                  <Show when="signed-in">
+                    <UserButton />
+                  </Show>
+        </ClerkProvider>
     </header>
   );
 }
